ea932a76c026f45781dbcb64731ea46a1be67d4b
mkcello96 <kershm@uw.edu>
2014-09-09 01:12:33 -0700

    Added a pwetty jPanel

--- NatureSounds/src/PlaySounds.java
+++ NatureSounds/src/PlaySounds.java
@@ -1,42 +1,60 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class PlaySounds {
 
+	public static Clip clip = null;
+
 	public static void main(String[] args) throws LineUnavailableException,
 			UnsupportedAudioFileException, IOException {
-		LinkedList<File> soundFiles = (LinkedList<File>) loadSounds();
+		final LinkedList<File> soundFiles = (LinkedList<File>) loadSounds();
 
-		Scanner scanner = new Scanner(System.in);
-		System.out
-				.println("Press enter to scroll through sounds. To quit, press \"q,\" then enter");
+		JPanel panel = new JPanel();
+		JButton soundButton = new JButton("New Sound");
+		panel.add(soundButton);
+		final JLabel jlabel = new JLabel("");
+		panel.add(jlabel);
+		soundButton.addActionListener(new ActionListener() {
 
-		File sound;
-		Clip clip = null;
-		String s = scanner.nextLine();
-		while (!s.equals("q")) {
-			sound = soundFiles.remove();
-			soundFiles.add(sound);
+			public void actionPerformed(ActionEvent event) {
+				if (clip != null) {
+					clip.close();
+				}
 
-			System.out.print(sound.getName());
-			clip = playSound(sound);
+				File sound = soundFiles.remove();
+				soundFiles.add(sound);
 
-			s = scanner.nextLine();
+				try {
+					clip = playSound(sound);
+					jlabel.setText(sound.getName());
+				} catch (LineUnavailableException
+						| UnsupportedAudioFileException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
-			clip.close();
-		}
-		System.out.println("Have a nice day :)");
-		System.exit(0);
+			}
+		});
+
+		JFrame frame = new JFrame("Click on the button to hear new sounds");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		frame.setSize(500, 80);
+		frame.setVisible(true);
 
 	}
 
